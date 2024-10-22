fcf6d883ffef33762767c7499dc10d79102201b5
ImperatricaLouisa <86868898768@mail.ru>
2024-10-22 17:43:19 +0200

    Вывод нужных атрибутов работника/работников

--- app/Http/Controllers/API/ApiWorkerController.php
+++ app/Http/Controllers/API/ApiWorkerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\WorkerResource;
 use App\Models\Worker;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,10 @@ class ApiWorkerController extends Controller
 {
     public function index() {
         $workers = Worker::all();
-        return response()->json([
-            'data' => $workers
-        ]);
+	    return WorkerResource::collection($workers)->resolve();
     }
+
+	public function show(Worker $worker) {
+		return WorkerResource::make($worker)->resolve();
+	}
 }
